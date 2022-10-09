2a3bf9f33ed737294baa63140f0f620a1a7d3dd3
paxmiler <paxmiler@gmail.com>
2022-10-09 13:39:53 +0900

    Initial Commit

---
+++ ToyChat/ToyChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToyChatApp.swift
+//  ToyChat
+//
+//  Created by Martin.Q on 2022/10/09.
+//
+
+import SwiftUI
+
+@main
+struct ToyChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
